570b5bf40953f8e17c7a1abfbf110a9e34713fc8
Cyle Riggs <beardedfoo@gmail.com>
2017-07-12 16:17:55 +0200

    go fmt

--- https-get/main.go
+++ https-get/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "io"
-  "log"
-  "net/http"
-  "os"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-  resp, err := http.Get("https://www.ietf.org/rfc/rfc2795.txt")
-  if err != nil {
-    log.Fatal(err)
-  } else {
-    defer resp.Body.Close()
-    io.Copy(os.Stdout, resp.Body)
-  }
+	resp, err := http.Get("https://www.ietf.org/rfc/rfc2795.txt")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer resp.Body.Close()
+		io.Copy(os.Stdout, resp.Body)
+	}
 }
